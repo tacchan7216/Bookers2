4b1ebbcae0bd90a4e46d106b832b054aa898acd0
tacchan7216 <104713690+tacchan7216@users.noreply.github.com>
2022-05-14 07:02:07 +0000

    [Finish]ファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'users/show'
+  get 'users/edit'
+  get 'homes/top'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
