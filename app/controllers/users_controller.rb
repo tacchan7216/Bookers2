58137d54e90dae7ce1f187b3d224d76a49513ca0
tacchan7216 <104713690+tacchan7216@users.noreply.github.com>
2022-05-17 05:20:45 +0000

    [Finish]機能とview

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:name,:profile_image,:body)
+      params.require(:user).permit(:name,:profile_image,:introduction)
     end
 end
